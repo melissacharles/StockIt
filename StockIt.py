3b97dc90ab79babd5b0074efa1a6bdf831bf29bf
Melissa Charles <melissacharles.py@gmail.com>
2022-12-06 15:20:36 -0500

    Recommendations table added

--- StockIt.py
+++ StockIt.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Apple!
+# StockIt Application
+Shown are the stock **closing price** and ***volume*** of Apple! 
+Bonus: Stock **Recommendations**
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'AAPL'
 
@@ -27,3 +27,8 @@ st.write("""
 ### Volume
 """)
 st.line_chart(tickerDf.Volume)
+
+st.write("""
+### Recent Market Ratings
+""")
+tickerData.recommendations
