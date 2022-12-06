1450722328a7977da5f0bbd1ddde440dacc0d60e
Melissa Charles <melissacharles.py@gmail.com>
2022-12-06 12:22:43 -0500

    Stock Price App with Steamlit and Python

---
+++ myapp.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Apple!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2012-12-6', end='2022-12-6')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+### Closing Price
+""")
+st.line_chart(tickerDf.Close)
+
+st.write("""
+### Volume
+""")
+st.line_chart(tickerDf.Volume)
